2cb1ca87aa067ad9ccdbf464801449d2eaf43c6d
최미래 <miraexhoi@gmail.com>
2023-07-14 23:27:10 +0900

    7/14

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,21 +1,31 @@
 package com.study.board.controller;
 
+import ch.qos.logback.core.model.Model;
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annoation.ResponseBody;
 
 @Controller
 public class BoardController {
+    @Autowired
+    private BoardService boardService;
     @GetMapping("/board/write") //localhost:8080/board/write
     public String boardWriteForm(){
+
         return "boardwrite";
     }
 
     @PostMapping("board/writePro")
-    public String boardWritePro(String title, String content) {
-        System.out.println("제목 : "+title);
-        System.out.println("내용 : "+content);
+    public String boardWritePro(Board board) {
+        boardService.write(board);
         return "";
+    }
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAtturibute("list", boardService.boardlist())
+        return "boardlist";
     }
 }
